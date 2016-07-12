d52bd4752e8955ad674072e7bcb1eee10fce0c73
Ahsan Ellahi <ahsan.ellahi@square63.com>
2016-07-12 14:06:42 +0500

    IntercomClient: Integrated completely

---
+++ app/models/client.rb
@@ -0,0 +1,130 @@
+class Client
+
+  # Intercom API Client
+  def initialize(organization)
+    @intercom_client = Intercom::Client.new(app_id: ENV['intercom_api_id'], api_key: ENV['intercom_api_key'])
+  end
+
+  def find_all(entity, created_since = nil)
+    method = "get_#{entity.pluralize}"
+    created_since = (Date.today - Date.parse(created_since)).to_i
+    send(method, created_since)
+  end
+
+  def get_users(created_since = nil)
+    users = if created_since
+              @intercom_client.users.find_all(created_since: created_since)
+            else
+              @intercom_client.users.all
+            end
+
+    entities = users.collect do |user|
+      {
+        id: user.id,
+        user_id: user.user_id,
+        email: user.email,
+        name: user.name,
+        location_data: {
+          city_name: user.location_data.city_name,
+          country_name: user.location_data.country_name,
+          postal_code: user.location_data.postal_code,
+          region_name: user.location_data.region_name
+        },
+        company_id: user.companies[0].id,
+      }
+    end
+
+    entities
+  end
+
+  def get_companies(created_since = nil)
+    companies = if created_since
+                  @intercom_client.companies.find_all(created_since: created_since)
+                else
+                  @intercom_client.companies.all
+                end
+
+    entities = companies.collect do |company|
+      {
+        id: company.id,
+        name: company.name,
+        user_count: company.user_count,
+        monthly_spend: company.monthly_spend,
+      }
+    end
+
+    entities
+  end
+
+  def get_contacts(created_since = nil)
+    contacts = if created_since
+                  @intercom_client.contacts.find_all(created_since: created_since)
+                else
+                  @intercom_client.contacts.all
+                end
+
+    entities = contacts.collect do |contact|
+      {
+        id: contact.id,
+        user_id: contact.user_id,
+        email: contact.email,
+        name: contact.name,
+        location_data: {
+          city_name: contact.location_data.city_name,
+          country_name: contact.location_data.country_name,
+          postal_code: contact.location_data.postal_code,
+          region_name: contact.location_data.region_name
+        },
+      }
+    end
+
+    entities
+  end
+
+  def create(entity, params)
+    method = "create_#{entity.pluralize}"
+    send(method, params)
+  end
+
+  def create_users(params)
+    @intercom_client.users.create(params)
+  end
+
+  def create_companies(params)
+    @intercom_client.companies.create(params)
+  end
+
+  def create_contacts(params)
+    @intercom_client.contacts.create(params)
+  end
+
+  def update(entity, entity_id, params)
+    method = "update_#{entity.pluralize}"
+    send(method, entity_id, params)
+  end
+
+  def update_users(entity_id, params)
+    @intercom_client.users.create(params)
+  end
+
+  def update_companies(entity_id, params)
+    @intercom_client.companies.create(params)
+  end
+
+  def update_contacts(entity_id, params)
+    begin
+      contact = @intercom_client.contacts.find(id: entity_id)
+    rescue
+      contact = @intercom_client.contacts.find(user_id: params[:user_id])
+    end
+
+    contact.user_id = params[:user_id]
+    contact.email = params[:email]
+    contact.location_data.city_name = params[:city_name]
+    contact.location_data.country_name = params[:country_name]
+    contact.location_data.postal_code = params[:postal_code]
+    contact.location_data.region_name = params[:region_name]
+
+    @intercom_client.contacts.save(contact)
+  end
+end
--- app/models/maestrano/connector/rails/entity.rb
+++ app/models/maestrano/connector/rails/entity.rb
@@ -10,41 +10,41 @@ class Maestrano::Connector::Rails::Entity < Maestrano::Connector::Rails::EntityB
   # Return an array of entities from the external app
   def get_external_entities(last_synchronization_date=nil)
     Maestrano::Connector::Rails::ConnectorLogger.log('info', @organization, "Fetching #{Maestrano::Connector::Rails::External.external_name} #{self.class.external_entity_name.pluralize}")
-    # TODO
-    # This method should return only entities that have been updated since the last_synchronization_date
-    # It should also implements an option to do a full synchronization when @opts[:full_sync] == true or when there is no last_synchronization_date
-    # It should also support [:__limit] and [:__skip] opts, meaning that if they are present, it should return only @[:__limit] entities while skipping the @opts[:__skip] firsts
-    # Maestrano::Connector::Rails::ConnectorLogger.log('info', @organization, "Received data: Source=#{Maestrano::Connector::Rails::External.external_name}, Entity=#{self.class.external_entity_name}, Response=#{entities}")
+
+    if @opts[:full_sync] || last_synchronization.blank?
+      entities = @external_client.find_all(self.class.external_entity_name)
+    else
+      entities = @external_client.find_all(self.class.external_entity_name, last_synchronization.updated_at)
+    end
+
+    Maestrano::Connector::Rails::ConnectorLogger.log('info', @organization, "Received data: Source=#{Maestrano::Connector::Rails::External.external_name}, Entity=#{self.class.external_entity_name}, Response=#{entities}")
+
+    entities
   end
 
   def create_external_entity(mapped_connec_entity, external_entity_name)
     Maestrano::Connector::Rails::ConnectorLogger.log('info', @organization, "Sending create #{external_entity_name}: #{mapped_connec_entity} to #{Maestrano::Connector::Rails::External.external_name}")
-    # TODO
-    # This method creates the entity in the external app and returns the external id
+
+    entity = @external_client.create(external_entity_name, mapped_connec_entity)
+    self.class.id_from_external_entity_hash(entity)
   end
 
   def update_external_entity(mapped_connec_entity, external_id, external_entity_name)
     Maestrano::Connector::Rails::ConnectorLogger.log('info', @organization, "Sending update #{external_entity_name} (id=#{external_id}): #{mapped_connec_entity} to #{Maestrano::Connector::Rails::External.external_name}")
-    # TODO
-    # This method updates the entity with the given id in the external app
+
+    @external_client.update(external_entity_name, external_id, mapped_connec_entity)
   end
 
   def self.id_from_external_entity_hash(entity)
-    # TODO
-    # This method return the id from an external_entity_hash
-    # e.g entity['id']
+    entity['id']
   end
 
   def self.last_update_date_from_external_entity_hash(entity)
-    # TODO
-    # This method return the last update date from an external_entity_hash
-    # e.g entity['last_update']
+    entity['updated_at'] || Time.now
   end
 
   def self.creation_date_from_external_entity_hash(entity)
-    # TODO
-    # This method return the creation date from an external_entity_hash
-    # e.g entity['created_at']
+    entity['created_at'] || Time.now
   end
 
   def self.inactive_from_external_entity_hash?(entity)
