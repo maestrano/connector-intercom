434358083ee109113dad8f93db9a5ce023d9f4df
Ahsan Ellahi <ahsan.ellahi@square63.com>
2016-07-13 18:08:53 +0500

    Company: Added mapping and rspec test case

--- app/models/maestrano/connector/rails/external.rb
+++ app/models/maestrano/connector/rails/external.rb
@@ -6,17 +6,10 @@ class Maestrano::Connector::Rails::External
   end
 
   def self.get_client(organization)
-    Client.new organization
+    IntercomClient.new organization
   end
 
-  # Return an array of all the entities that the connector can synchronize
-  # If you add new entities, you need to generate
-  # a migration to add them to existing organizations
   def self.entities_list
-    # TODO
-    # The names in this list should match the names of your entities class
-    # e.g %w(person, tasks_list)
-    #  will synchronized Entities::Person and Entities::TasksList
-    []
+    %w(company)
   end
 end
---
+++ spec/models/entities/company_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe Entities::Company do
+
+  describe 'class methods' do
+    subject { Entities::Company }
+
+    it { expect(subject.connec_entity_name).to eql('Organization') }
+    it { expect(subject.external_entity_name).to eql('Company') }
+    it { expect(subject.object_name_from_connec_entity_hash({'name' => 'Ahsan'})).to eql('Ahsan') }
+    it { expect(subject.object_name_from_external_entity_hash({'name' => 'Ahsan'})).to eql('Ahsan') }
+  end
+
+  describe 'instance methods' do
+    let(:organization) { create(:organization) }
+    let(:connec_client) { Maestrano::Connector::Rails::ConnecHelper.get_client(organization) }
+    let(:external_client) { Maestrano::Connector::Rails::External.get_client(organization) }
+    let(:opts) { {} }
+    subject { Entities::Company.new(organization, connec_client, external_client, opts) }
+
+    describe 'external to connec!' do
+      let(:external_hash) {
+        {
+          'id' => '531ee472cce572a6ec000006',
+          'name' => 'Blue',
+          'user_count' => 10,
+          'monthly_spend' => 49,
+          'created_at' => 1394533506,
+          'updated_at' => 1396874658,
+        }
+      }
+
+      let (:mapped_external_hash) {
+        {
+          'name' => 'Blue',
+          'number_of_employees' => 10,
+          'capital' => 49,
+          'id' => [{'id' => '531ee472cce572a6ec000006', 'provider' => 'this_app', 'realm' => 'sfuiy765'}],
+        }.with_indifferent_access
+      }
+
+      it { expect(subject.map_to_connec(external_hash)).to eql(mapped_external_hash) }
+    end
+
+    describe 'connec to external' do
+      let(:connec_hash) {
+        {
+          'name' => 'Blue',
+          'number_of_employees' => 10,
+          'capital' => 49,
+        }
+      }
+
+      let(:mapped_connec_hash) {
+        {
+          'name' => 'Blue',
+          'user_count' => 10,
+          'monthly_spend' => 49,
+        }.with_indifferent_access
+      }
+
+      it { expect(subject.map_to_external(connec_hash)).to eql(mapped_connec_hash) }
+    end
+  end
+end
