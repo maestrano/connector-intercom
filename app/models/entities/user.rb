8afb2be1ad769516952b3b51bbd21f2ae3275830
Ahsan Ellahi <ahsan.ellahi@square63.com>
2016-07-14 17:34:05 +0500

    User: Implemented mapping and rspec test cases

---
+++ app/models/entities/user.rb
@@ -0,0 +1,59 @@
+class Entities::User < Maestrano::Connector::Rails::Entity
+  def self.connec_entity_name
+    'People'
+  end
+
+  def self.external_entity_name
+    'User'
+  end
+
+  def self.mapper_class
+    UserMapper
+  end
+
+  def self.object_name_from_connec_entity_hash(entity)
+    [entity['first_name'], entity['last_name']].join(' ')
+  end
+
+  def self.object_name_from_external_entity_hash(entity)
+    entity['name']
+  end
+
+  def self.references
+    %w(organization_id)
+  end
+end
+
+class UserMapper
+  extend HashMapper
+
+  # Mapping to Intercom
+  after_normalize do |input, output|
+    output[:name] = [input['first_name'], input['last_name']].join(' ').to_s.strip
+    output
+  end
+
+  # Mapping to Connec!
+  after_denormalize do |input, output|
+    if input['name']
+      output[:first_name] = input['name'].split(' ', 2).first
+      output[:last_name] = input['name'].split(' ', 2).last
+    end
+
+    output[:is_customer] = true
+    output[:is_lead] = false
+
+    output
+  end
+
+  map from('id'), to('id')
+  map from('code'), to('user_id')
+  map from('email/address'), to('email')
+
+  map from('address_work/billing/city'), to('location_data/city_name')
+  map from('address_work/billing/country'), to('location_data/country_name')
+  map from('address_work/billing/postal_code'), to('location_data/postal_code')
+  map from('address_work/billing/region'), to('location_data/region_name')
+
+  map from('organization_id'), to('companies[0]/id')
+end
